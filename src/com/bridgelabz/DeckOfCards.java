b51404ae7cb99a304952f42cea2f5c581aa5e7fb
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-02 18:53:15 +0530

    Deck Initialization

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -2,6 +2,18 @@ package com.bridgelabz;
 
 public class DeckOfCards {
     public static void main(String[] args) {
-        System.out.println("-- Deck of Cards --");
+        System.out.println("-- Deck of Cards --\n");
+
+        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King", "Ace"};
+        String[][] cardArray = new String[4][13];
+
+        for(int i = 0; i < 4; i++) {
+            System.out.println();
+            for (int j = 0; j < 13; j++) {
+                cardArray[i][j] = suit[i] + ' ' + rank[j];
+                System.out.println(cardArray[i][j]);
+            }
+        }
     }
 }
